2ebf4bae1b64013e6542a818d4ab0eaef1547596
monger18 <satyamsri1809@gmail.com>
2024-01-17 19:57:03 +0530

    first commit

---
+++ Downloads/Dijkstra-main/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualiser';
+function App(){
+   return (
+<div className="App">
+  <PathfindingVisualizer></PathfindingVisualizer>
+</div>
+   );
+}
+export default App;
